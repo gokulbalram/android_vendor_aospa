AOSPA/android_vendor_pa

pa: products: Add OnePlus 8/Pro.
 * Also known as "instantnoodle", or "instantnoodlep".

Change-Id: I63ce4e9dfedfc9472cd10ca947529a525a0abe18

products/AndroidProducts.mk
@@ -38,6 +38,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/oneplus7pro/pa_oneplus7pro.mk \
     $(LOCAL_DIR)/oneplus7t/pa_oneplus7t.mk \
     $(LOCAL_DIR)/oneplus7tpro/pa_oneplus7tpro.mk \
+    $(LOCAL_DIR)/oneplus8/pa_oneplus8.mk \
     $(LOCAL_DIR)/polaris/pa_polaris.mk \
     $(LOCAL_DIR)/raphael/pa_raphael.mk \
     $(LOCAL_DIR)/rolex/pa_rolex.mk \
@@ -72,6 +73,7 @@ COMMON_LUNCH_CHOICES += \
     pa_oneplus7pro-userdebug \
     pa_oneplus7t-userdebug \
     pa_oneplus7tpro-userdebug \
+    pa_oneplus8-userdebug \
     pa_polaris-userdebug \
     pa_rolex-userdebug \
     pa_rosy-userdebug \

gokulbalram/android_vendor_aospa

products/AndroidProducts.mk
@@ -25,7 +25,8 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/oriole/aospa_oriole.mk \
     $(LOCAL_DIR)/panther/aospa_panther.mk \
     $(LOCAL_DIR)/phoenix/aospa_phoenix.mk \
-    $(LOCAL_DIR)/raven/aospa_raven.mk
+    $(LOCAL_DIR)/raven/aospa_raven.mk \
+    $(LOCAL_DIR)/raven/aospa_oneplus8.mk \
 
 # Lunch targets
 COMMON_LUNCH_CHOICES += \
@@ -40,4 +41,6 @@ COMMON_LUNCH_CHOICES += \
     aospa_oriole-userdebug \
     aospa_panther-userdebug \
     aospa_phoenix-userdebug \
-    aospa_raven-userdebug
+    aospa_raven-userdebug \
+    aospa_oneplus8-userdebug
+